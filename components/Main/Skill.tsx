829bdd2d4c22a40f1e6ad0718fb31fbec389d30f
정찬영 <jcy3589@gmail.com>
2022-07-16 14:14:00 +0000

    Skill Hover적용

--- components/Main/Skill.tsx
+++ components/Main/Skill.tsx
@@ -71,28 +71,26 @@ const Skill: FC<SkillProps> = ({ skill }) => {
   );
 };
 
-function GridItem({ src, title }: MainSkillImage) {
-  return (
-    <Box w="100%" textAlign="center">
-      <LinkBox cursor="pointer">
-        <SkillImageWrapper>
-          <Image
-            src={src}
-            alt={title ?? 'skill-image'}
-            style={{ borderRadius: '12px' }}
-            loading="lazy"
-            objectFit="cover"
-            layout="fill"
-            unoptimized
-          />
-        </SkillImageWrapper>
-        <Badge fontSize={14} mt={4}>
-          {title}
-        </Badge>
-      </LinkBox>
-    </Box>
-  );
-}
+const GridItem = ({ src, title }: MainSkillImage) => (
+  <Box w="100%" textAlign="center">
+    <LinkBox _hover={{ transform: 'scale(1.1)' }} transition="0.3s ease">
+      <SkillImageWrapper>
+        <Image
+          src={src}
+          alt={title ?? 'skill-image'}
+          style={{ borderRadius: '12px' }}
+          loading="lazy"
+          objectFit="cover"
+          layout="fill"
+          unoptimized
+        />
+      </SkillImageWrapper>
+      <Badge fontSize={14} mt={4}>
+        {title}
+      </Badge>
+    </LinkBox>
+  </Box>
+);
 const SkillImageWrapper = styled.div`
   width: 100%;
   padding-bottom: 100%;
