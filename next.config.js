cf801c767fea8c700ffb387b6e727196f58d8aff
chad <jcy3589@gmail.com>
2023-08-07 15:54:48 +0900

    rewrites (solved ac api)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
@@ -14,6 +13,12 @@ const nextConfig = {
   },
   compress: true,
   productionBrowserSourceMaps: false,
+  rewrites: () => [
+    {
+      destination: 'https://solved.ac/api/v3/:path*',
+      source: '/api/solved/:path*',
+    },
+  ],
 };
 
 module.exports = withPlugins([[withBundleAnalyzer], nextConfig]);
