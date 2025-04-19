1785ed59c051b2058971c3e7d0f4e40d663f73e4
chad <jcy3589@gmail.com>
2025-04-19 21:01:51 +0900

    fix: fix vercel build error attempted

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
+  swcMinify: false,
   i18n: {
     locales: ['en', 'ko'],
     defaultLocale: 'ko',
